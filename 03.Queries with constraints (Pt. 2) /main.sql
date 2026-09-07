a99e1b5f735d24eb69c6a16fe184674370677105
Tanvir Ahammad <tanvir-007@kali.tanvir>
2026-09-07 12:46:11 -0400

    day-1

---
+++ 03.Queries with constraints (Pt. 2) /main.sql
@@ -0,0 +1,15 @@
+-- Find all the Toy Story movies
+SELECT * FROM movies
+WHERE title like '%Toy Story%';
+
+-- Find all the movies directed by John Lasseter
+SELECT * FROM movies
+WHERE director like '%John Lasseter%';
+
+-- Find all the movies (and director) not directed by John Lasseter
+SELECT * FROM movies
+WHERE director not like '%John Lasseter%';
+
+-- Find all the WALL-* movies
+SELECT * FROM movies 
+WHERE title LIKE '%WALL-%';
